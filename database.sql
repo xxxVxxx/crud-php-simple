1a8af9803acb5bbee52b8031d52a4b819c7bd9df
Madhav <maddy4chat@gmail.com>
2019-06-04 20:44:22 +0530

    added a few entries to database

--- database.sql
+++ database.sql
@@ -5,3 +5,9 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   PRIMARY KEY  (`id`)
 );
+
+INSERT INTO users (name,age,email) VALUES ('madhav', 33, 'maddy4chat@gmail.com');
+INSERT INTO users (name, age, email) VALUES ('maddy', 28, 'maddy@gmail.com');
+INSERT INTO users (name,age,email) VALUES ('sandy', 14, 'sandy14@gmail.com');
+INSERT INTO users (name,age,email) VALUES ('albert', 28, 'albertyo@gmail.com');
+
